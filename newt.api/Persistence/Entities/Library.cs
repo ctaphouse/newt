d2293b697faca5164e9df7725c350e2cbcb84194
Green Hornet <taphousecarl@gmail.com>
2022-04-26 11:04:13 -0400

    add entities and data context class

---
+++ newt.api/Persistence/Entities/Library.cs
@@ -0,0 +1,9 @@
+namespace newt.api.Persistence.Entities;
+
+public class Library
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = default!;
+
+    public ICollection<Book> Books { get; set; } = new List<Book>();
+}
